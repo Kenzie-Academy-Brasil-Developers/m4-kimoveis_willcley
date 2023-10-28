f5f9485f51b53637ab52e6694d4cb01cc070e2f4
Willcley Melo <willcley2003@gmail.com>
2023-10-28 11:21:15 -0300

    feat: create controllers

---
+++ src/controllers/session.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+import { loginService } from '../services/session.service';
+import { TLoginReturn } from '../interfaces/users.interface';
+
+export const loginController = async (req: Request, res: Response): Promise<Response> => {
+    const token: TLoginReturn = await loginService(req.body);
+
+    return res.status(200).json(token);
+};
