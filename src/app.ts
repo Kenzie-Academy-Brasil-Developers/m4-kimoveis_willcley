b59e7cbc36de79212ca7c1fee2fc3a85a3736d74
Willcley Melo <willcley2003@gmail.com>
2023-10-28 11:15:55 -0300

    feat: create app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import 'reflect-metadata';
 import 'express-async-errors';
-import express from 'express';
+import express, { Application } from 'express';
+import { handleError } from './middlewares/handleErrors.middleware';
+import { routers } from './routers/index';
 
-const app = express();
+const app: Application = express();
 app.use(express.json());
+
+app.use('/', routers);
+
+app.use(handleError);
 
 export default app;
